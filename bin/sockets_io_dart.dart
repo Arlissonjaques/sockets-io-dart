ee2f7a71ad6650bc624e9aa01b83826345916162
Arlisson Jaques <arlissonjaques@gmail.com>
2023-02-27 20:46:10 -0300

    init project

---
+++ bin/sockets_io_dart.dart
@@ -0,0 +1,5 @@
+import 'package:sockets_io_dart/sockets_io_dart.dart' as sockets_io_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sockets_io_dart.calculate()}!');
+}
---
+++ test/sockets_io_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:sockets_io_dart/sockets_io_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
